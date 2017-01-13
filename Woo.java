eb27d795a9be1b648d189784d342d361bcf0f4de
srafi1 <shakilrafi0@gmail.com>
2017-01-12 21:06:53 -0500

    driver file expanded

--- Woo.java
+++ Woo.java
@@ -6,15 +6,68 @@ public class Woo  {
 	System.out.println("Welcome to Line Forge!");
 	
 	Keyboard in = new Keyboard();
+	AxisGraph graph = new AxisGraph();
+	
+	String helpText = "";
+	helpText += "help -- displays this help text\n";
+	helpText += "quit (or exit) -- exits the program\n\n";
+	helpText += "Enter an expression -- evaluates expression\n";
+	helpText += "    eg: input: 2*(1+1)\n";
+	helpText += "        output: 4.0\n";
+	helpText += "Enter an equation -- graphs the equation\n";
+	helpText += "    eg: input: y=x\n";
+	helpText += "        output: graph of y=x\n\n";
+	helpText += "After graphing, you can use these commands:\n";
+	helpText += "zoom [high] -- zooms in or out on the graph so that the parameter\n";
+	helpText += "               is the highest value along each axis\n";
+	helpText += "    eg: input: zoom 30\n";
+	helpText += "        output: the equation is graphed from [-30,30] along both axes\n";
+	helpText += "translate [x] [y] -- moves the graph over\n";
+	helpText += "    eg: input: translate 1 -2\n";
+	helpText += "        output: moves graph 1 point left and 2 down\n";
+
+	boolean graphMode = false;
+	String eq = "";
+	double highVal = 10;
+	
 	while (true) {
-	    System.out.print("What to do? (input 'help' for help)\n>");
+	    System.out.print("What to do...? (type 'help' for help)\n>");
 	    String input = in.readString();
 
-	    if (input.equals("quit"))
+	    if (input.equals("quit") || input.equals("exit"))
 		break;
 	    else if (input.equals("help"))
-		System.out.println("Enter an expression to evaluate it or 'quit' to quit");
-	    else {
+		System.out.println(helpText);
+	    else if (input.indexOf("=") != -1 && (input.indexOf("y") != -1 || input.indexOf("x") != -1)) {
+		try {
+		    eq = input;
+		    graph.graph(input);
+		    System.out.println(graph);
+		    graphMode = true;
+		} catch (Exception e) {		    
+		    System.out.println("A voice asks you to rethink your actions");
+		}
+	    } else if (graphMode && input.indexOf("zoom") == 0) {
+		//zoom
+		double scale = Double.parseDouble(input.substring(5));
+		graph.zoom(scale);
+		graph.graph(eq);
+		highVal = scale;
+		System.out.println(graph);
+	    } else if (graphMode && input.indexOf("translate") == 0) {
+		//translate
+		String params = input.substring(10);
+		String[] coords = params.split(" ");
+		double dx = Double.parseDouble(coords[0]);
+		double dy = Double.parseDouble(coords[1]);
+
+		//graph.translate(dx, dy);
+		graph.graph(eq);
+		System.out.println(graph);
+	    } else if (input.equals("status")) {
+		System.out.println("Graph mode: " + graphMode);
+		System.out.println("Highest Value: " + highVal);
+	    } else {
 		try {
 		    System.out.println(MathString.pemdas(input));
 		} catch(Exception e) {
@@ -22,6 +75,8 @@ public class Woo  {
 		}
 	    }
 	}
+
+	System.out.println();
     }
     
 }
