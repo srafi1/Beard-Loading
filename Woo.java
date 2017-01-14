9af8288afceccdf8b81b2bee076cf95a62d9001a
srafi1 <shakilrafi0@gmail.com>
2017-01-14 12:31:21 -0500

    made driver file more intuitive

--- Woo.java
+++ Woo.java
@@ -4,6 +4,8 @@ public class Woo  {
     
     public static void main(String[] args) {
 	System.out.println("Welcome to Line Forge!");
+	System.out.println("Enter an expression or equation to begin!");
+	System.out.println();
 	
 	Keyboard in = new Keyboard();
 	AxisGraph graph = new AxisGraph();
@@ -29,7 +31,7 @@ public class Woo  {
 	helpText += "         ie: (zoom 20, no translations)\n";
 	helpText += "status -- prints out information about the current state of the graph\n";
 	helpText += "          ie: zoom level, translations, equation\n";
-	helpText += "NOTE: Use '~' instead of '-' for negatives\n";
+	helpText += "NOTE: Use '~' instead of '-' for negative numbers\n";
 
 	boolean graphMode = false;
 	String eq = "";
@@ -38,23 +40,14 @@ public class Woo  {
 	double totaldy = 0;
 	
 	while (true) {
-	    System.out.print("What to do...? (type 'help' for help)\n>");
+	    System.out.print("What to do...? (input 'help' for help or 'quit' to exit)\n>");
 	    String input = in.readString();
 
 	    if (input.equals("quit") || input.equals("exit"))
 		break;
 	    else if (input.equals("help"))
 		System.out.println(helpText);
-	    else if (input.indexOf("=") != -1 && (input.indexOf("y") != -1 || input.indexOf("x") != -1)) {
-		try {
-		    eq = input;
-		    graph.graph(input);
-		    System.out.println(graph);
-		    graphMode = true;
-		} catch (Exception e) {		    
-		    System.out.println("A voice asks you to rethink your actions");
-		}
-	    } else if (graphMode && input.indexOf("zoom") == 0) {
+	    else if (graphMode && input.indexOf("zoom") == 0) {
 		//zoom
 		try {
 		    double scale = Double.parseDouble(input.substring(5));
@@ -64,7 +57,7 @@ public class Woo  {
 		    highVal = scale;
 		    System.out.println(graph);
 		} catch (Exception e) {
-		    System.out.println("A voice asks you to rethink your actions");
+		    System.out.println("Use zoom in the format: zoom [scale]");
 		}
 	    } else if (graphMode && input.indexOf("translate") == 0) {
 		//translate
@@ -82,7 +75,7 @@ public class Woo  {
 
 		    System.out.println(graph);
 		} catch (Exception e) {
-		    System.out.println("A voice asks you to rethink your actions");
+		    System.out.println("Use translate in the format: translate [change in x] [change in y]");
 		}
 	    } else if (graphMode && input.indexOf("reset") == 0) {
 		graph.translate(-1*totaldx, -1*totaldy);
@@ -97,11 +90,28 @@ public class Woo  {
 		System.out.println("Graph mode: " + graphMode);
 		System.out.println("Zoom level: " + highVal);
 		System.out.println("Total translations: " + totaldx + " " + totaldy);
+	    } else if (input.indexOf("=") != -1 && (input.indexOf("y") != -1 || input.indexOf("x") != -1)) {
+		try {
+		    input = input.replace("X","x");
+		    input = input.replace("Y","y");
+
+		    eq = input;
+		    graph.graph(input);
+
+		    System.out.println(graph);
+		    graphMode = true;
+		    System.out.println("Now you can use the 'zoom [scale]' and 'translate [x] [y]' commands");
+		} catch (Exception e) {		    
+		    System.out.println("The equation: " + input + " is invalid. Make sure it includes at least one x or y");
+		    System.out.println();
+		}
 	    } else {
 		try {
 		    System.out.println(MathString.pemdas(input));
 		} catch(Exception e) {
-		    System.out.println("A voice asks you to rethink your actions");
+		    System.out.println("The expression: " + input + " is invalid");
+		    if (input.indexOf("--") != -1)
+			System.out.println("Remember: use '~' for negative numbers instead of '-'");
 		}
 	    }
 	}
